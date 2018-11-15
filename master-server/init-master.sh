ef920fe36e7261eb16a7a535e2fd280f89a2be07
bgulowaty <b.gulowaty@gmail.com>
2018-11-15 01:11:31 +0100

    boże

--- master-server/init-master.sh
+++ master-server/init-master.sh
@@ -2,12 +2,30 @@
 set -e
 
 echo "Creating replication role $REPLICATION_USER"
+echo "host replication repuser 0.0.0.0/0 scram-sha-256" >> "$PGDATA/pg_hba.conf"
 
-gosu postgres postgres --single <<EOSQL
-  SET password_encryption = 'scram-sha-256';
-    CREATE ROLE repuser WITH REPLICATION PASSWORD 'password' LOGIN;
-    SELECT * FROM pg_create_physical_replication_slot('replica_1_slot');
-    SELECT * FROM pg_create_physical_replication_slot('replica_2_slot');
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+SET password_encryption = 'scram-sha-256';
+CREATE ROLE repuser WITH REPLICATION PASSWORD '$REPLICATION_PASSWORD' LOGIN;
+SELECT * FROM pg_create_physical_replication_slot('replica_1_slot');
+SELECT * FROM pg_create_physical_replication_slot('replica_2_slot');
 EOSQL
 
-mv /tmp/conf/postgresql.conf /var/lib/postgresql/data/postgresql.conf
+cat >> ${PGDATA}/postgresql.conf <<EOF
+
+wal_level = hot_standby
+
+archive_mode = on
+
+archive_command = 'cd .'
+
+max_wal_senders = 8
+
+wal_keep_segments = 8
+
+hot_standby = on
+
+EOF
+
+cat /tmp/conf/postgresql.conf > "$PGDATA/postgresql.conf"
+
--- slave-db/init-slave.sh
+++ slave-db/init-slave.sh
@@ -1,8 +1,45 @@
 #!/bin/bash
 set -e
 
+until ping -c 1 -W 1 db-master
+do
+    echo "Waiting for master to ping..."
+    sleep 1s
+done
 
+echo "*:*:*:$REPLICATION_USER:$REPLICATION_PASSWORD" > ~/.pgpass
+chmod 0600 ~/.pgpass
+echo $PGPASSWORD
+export PGPASSWORD="$REPLICATION_PASSWORD"
 
-mv /tmp/conf/pg_hba.conf /var/lib/postgresql/data/pg_hba.conf
+rm -rf {PGDATA}
+until pg_basebackup -h db-master -D ${PGDATA} -U ${REPLICATION_USER} -vP
+do
+cat ~/.pgpass
+echo $REPLICATION_USER
+echo $PGPASSWORD
+echo "Waiting for master to connect..."
+rm -rf /var/lib/postgresql/data/*
 
-# pg_basebackup -h <PRIMARY_IP> -D <DATA_DIRECTORY> -U repuser -vP -W
+sleep 4s
+
+done
+
+cat > ${PGDATA}/recovery.conf <<EOF
+
+standby_mode = on
+primary_conninfo = 'host=db-master port=5432 user=$REPLICATION_USER password=password application_name=r1'
+primary_slot_name = 'replica_1_slot' # Name of the replication slot we created on the master
+
+EOF
+chmod 0600 "${PGDATA}/recovery.conf"
+
+cat >> ${PGDATA}/postgresql.conf <<EOF
+
+hot_standby = on
+wal_level = replica
+max_wal_senders = 2
+max_replication_slots = 2
+synchronous_commit = off
+
+EOF
