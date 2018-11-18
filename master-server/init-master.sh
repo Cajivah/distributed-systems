7939411677275ee633392e197f82bb4b25299aa1
Bogdan Gulowaty <b.gulowaty@gmail.com>
2018-11-18 20:38:10 +0100

    Add multiple slaves support

--- master-server/init-master.sh
+++ master-server/init-master.sh
@@ -1,32 +1,44 @@
 #!/bin/bash
 set -e
 
-# Add support for multiple replicas
+replicas_count=0
 
-# Allow replication user to connect
-# TODO(bgulowaty): current mask allows all addresses
-# TODO(bgulowaty): should allow only replica address 
-echo "host replication $REPLICATION_USER 0.0.0.0/0 scram-sha-256" >> "$PGDATA/pg_hba.conf"
+for replication in $(cat $REPLICATION_USERS_FILE); do
+    USER=$(echo $replication | cut -d ":" -f2)
+    PASS=$(echo $replication | cut -d ":" -f3)
+    
+    echo Adding $USER with $PASS
+    # Allow replication user to connect
+    # TODO(bgulowaty): current mask allows all addresses
+    # TODO(bgulowaty): should allow only replica address 
+    echo "host replication $USER 0.0.0.0/0 scram-sha-256" >> "$PGDATA/pg_hba.conf"
 
-# Create replication user
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-SET password_encryption = 'scram-sha-256';
-CREATE ROLE $REPLICATION_USER WITH REPLICATION PASSWORD '$REPLICATION_PASSWORD' LOGIN;
-EOSQL
+    # Create replication user
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	SET password_encryption = 'scram-sha-256';
+	CREATE ROLE $USER WITH REPLICATION PASSWORD '$PASS' LOGIN;
+	EOSQL
+    replicas_count=$((replicas_count+1))
+done;
 
 # Configure master
 cat >> ${PGDATA}/postgresql.conf <<EOF
 listen_addresses= '*'
 wal_level = replica
-max_wal_senders = 2
-max_replication_slots = 2
+max_wal_senders = $((replicas_count+2))
+max_replication_slots = $replicas_count
 synchronous_commit = off
 EOF
 
 # Restart server
 pg_ctl -D ${PGDATA} -m fast -w restart
 
-# Add replica slot (must be executed after restart)
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-SELECT * FROM pg_create_physical_replication_slot('replica_1_slot');
-EOSQL
+# Add replica slots (must be executed after restart)
+for replication in $(cat $REPLICATION_USERS_FILE); do
+	SLOT=$(echo $replication | cut -d ":" -f1)
+	echo Adding replication slot $SLOT
+
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+	SELECT * FROM pg_create_physical_replication_slot('$SLOT');
+	EOSQL
+done;
--- slave-db/init-slave.sh
+++ slave-db/init-slave.sh
@@ -6,14 +6,14 @@ pg_ctl -D ${PGDATA} -m fast -w stop
 rm -rf /var/lib/postgresql/data/*
 
 # Wait for master to become online
-until ping -c 1 -W 1 ${MASTER_ADDRESS}
+until ping -c 1 -W 1 ${REPLICATION_MASTER_ADDRESS}
 do
     echo "Waiting for master to ping..."
     sleep 1s
 done
 
 # Add master connection credentials to .pgpass file
-# So we don't get asked in prompt for it
+# So we don't get prompted for it
 # TODO improve
 echo "*:*:*:$REPLICATION_USER:$REPLICATION_PASSWORD" > ~/.pgpass
 chown postgres:postgres ~/.pgpass
@@ -21,7 +21,7 @@ chmod 0600 ~/.pgpass
 echo $PGPASSWORD
 export PGPASSWORD="$REPLICATION_PASSWORD"
 
-until pg_basebackup -h ${MASTER_ADDRESS} -D ${PGDATA} -U ${REPLICATION_USER} -vP -w
+until pg_basebackup -h ${REPLICATION_MASTER_ADDRESS} -D ${PGDATA} -U ${REPLICATION_USER} -S ${REPLICATION_SLOT} -vP -w
 do
     echo "Trying to fetch pg_basebackup from master..."
     sleep 2s
@@ -30,10 +30,10 @@ done
 # Set replication configuration
 cat > ${PGDATA}/recovery.conf <<EOF
 standby_mode = on
-primary_conninfo = 'host=$MASTER_ADDRESS port=$MASTER_PORT user=$REPLICATION_USER password=$REPLICATION_PASSWORD application_name=r1'
-primary_slot_name = '$MASTER_REPLICATION_SLOT'
+primary_conninfo = 'host=$REPLICATION_MASTER_ADDRESS port=$REPLICATION_MASTER_PORT user=$REPLICATION_USER password=$REPLICATION_PASSWORD application_name=r1'
+primary_slot_name = '$REPLICATION_SLOT'
 EOF
-cat ${PGDATA}/recovery.conf
+
 # Adjust permissions
 chown postgres:postgres ${PGDATA}/recovery.conf
 chmod 0600 ${PGDATA}/recovery.conf
