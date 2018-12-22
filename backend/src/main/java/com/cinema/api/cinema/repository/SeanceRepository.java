f4252bc6372b9f9d29ed7f4081786298b72c763a
Aleksander Drozd <aleksander.drozd@outlook.com>
2018-12-22 14:06:01 +0100

    Minor refactor

--- backend/src/main/java/com/cinema/api/cinema/repository/SeanceRepository.java
+++ backend/src/main/java/com/cinema/api/cinema/repository/SeanceRepository.java
@@ -5,11 +5,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
 
 @Repository
 public interface SeanceRepository extends JpaRepository<Seance, Long> {
+
+     default List<Seance> findByCinemaIdAndDate(long cinemaId, LocalDate date) {
+          ZonedDateTime dayStart = ZonedDateTime.of(date, LocalTime.MIN, ZoneId.systemDefault());
+          ZonedDateTime dayEnd = ZonedDateTime.of(date, LocalTime.MAX, ZoneId.systemDefault());
+          return findByCinemaIdAndDateBetween(cinemaId, dayStart, dayEnd);
+     }
 
      @Query("select seance from Seance seance where room.cinema.id = ?1 and start between ?2 and ?3")
      List<Seance> findByCinemaIdAndDateBetween(long cinemaId, ZonedDateTime start, ZonedDateTime end);
--- backend/src/main/java/com/cinema/api/cinema/service/ProgrammeService.java
+++ backend/src/main/java/com/cinema/api/cinema/service/ProgrammeService.java
@@ -1,6 +1,7 @@
 package com.cinema.api.cinema.service;
 
 import com.cinema.api.cinema.mapper.SeanceMapper;
+import com.cinema.api.cinema.model.dto.BasicSeanceDTO;
 import com.cinema.api.cinema.model.dto.MovieWithSeancesDTO;
 import com.cinema.api.cinema.model.entity.Movie;
 import com.cinema.api.cinema.model.entity.Seance;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.*;
@@ -22,14 +24,17 @@ public class ProgrammeService {
 
      public List<MovieWithSeancesDTO> getProgramme(long cinemaId, LocalDate date) {
           List<Seance> seances = seanceService.getByCinemaIdAndDate(cinemaId, date);
-          return seances.stream()
-                        .collect(groupingBy(Seance::getMovie, mapping(seanceMapper::toSimpleSeance, toList())))
-                        .entrySet()
-                        .stream()
-                        .map(entry -> {
-                             Movie movie = entry.getKey();
-                             return new MovieWithSeancesDTO(movie.getId(), movie.getTitle(), entry.getValue());
-                        })
-                        .collect(Collectors.toList());
+          Map<Movie, List<BasicSeanceDTO>> seancesGroupedByMovie
+               = seances.stream()
+                        .collect(groupingBy(Seance::getMovie, mapping(seanceMapper::toSimpleSeance, toList())));
+
+          return seancesGroupedByMovie
+               .entrySet()
+               .stream()
+               .map(entry -> {
+                    Movie movie = entry.getKey();
+                    return new MovieWithSeancesDTO(movie.getId(), movie.getTitle(), entry.getValue());
+               })
+               .collect(Collectors.toList());
      }
 }
