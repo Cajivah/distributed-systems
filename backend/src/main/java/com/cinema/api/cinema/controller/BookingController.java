d0a42121f160d4b695e13a195c70db6ea9ffcdaa
Jakub Małyjasiak <malyjasiak@gmail.com>
2019-01-07 03:04:26 +0100

    Fix request mapping for booking deletion.

--- backend/src/main/java/com/cinema/api/cinema/controller/BookingController.java
+++ backend/src/main/java/com/cinema/api/cinema/controller/BookingController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,8 +32,9 @@ public class BookingController {
      }
 
      @DeleteMapping
+     @RequestMapping("/{identifier}")
      @ResponseStatus(HttpStatus.NO_CONTENT)
-     public void delete(@RequestParam String identifier) {
+     public void delete(@PathVariable String identifier) {
           bookingService.delete(identifier);
      }
 }
