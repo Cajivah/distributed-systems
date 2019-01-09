0672167bba6f11ed58785aa55ab7621253e28529
Jakub Małyjasiak <malyjasiak@gmail.com>
2019-01-09 23:27:07 +0100

    Add endpoint to fetch rooms by cinema id.

--- backend/src/main/java/com/cinema/api/cinema/repository/RoomRepository.java
+++ backend/src/main/java/com/cinema/api/cinema/repository/RoomRepository.java
@@ -1,9 +1,13 @@
 package com.cinema.api.cinema.repository;
 
 import com.cinema.api.cinema.model.entity.Room;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    Page<Room> findAllByCinema_Id(Long cinemaId, Pageable pageable);
 }
